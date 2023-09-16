d870bb56fb032af780fb03ad6d73ad6e9f96a45a
astherath <farceriv@gmail.com>
2023-09-15 20:36:01 -0400

    added code

---
+++ rs-daemon/src/filesystem.rs
@@ -0,0 +1,47 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use crate::models::{ProcessData, FsResult};
+use serde_json;
+
+pub fn make_process_file(data: ProcessData) -> FsResult<()> {
+    write_to_file(&data.to_output())?;
+    Ok(())
+}
+
+pub fn get_process() -> FsResult<ProcessData> {
+    let process = deserialize_from_file()?;
+    Ok(process)
+}
+
+fn deserialize_from_file() -> FsResult<ProcessData> {
+    let contents = read_from_file()?;
+    let data = serde_json::from_str(&contents)?;
+    Ok(data)
+}
+
+fn get_file_path() -> String {
+    "/Users/felipearce/Desktop/projects/shellhacks2023/daemon/rs-daemon/test.txt".to_string()
+}
+
+fn read_from_file() -> FsResult<String> {
+    let file_path_str = get_file_path();
+    let mut contents = String::new();
+
+    let mut file = File::open(file_path_str)?;
+    file.read_to_string(&mut contents)?;
+
+    Ok(contents)
+}
+
+fn write_to_file(content: &str) -> FsResult<()> {
+    let file_path_str = get_file_path();
+    let file_path = Path::new(&file_path_str);
+
+    let mut file = match file_path.exists() {
+        true => File::open(file_path_str)?,
+        false => File::create(file_path_str)?,
+    };
+    file.write(content.as_bytes())?;
+    Ok(())
+}
--- rs-daemon/src/main.rs
+++ rs-daemon/src/main.rs
@@ -1,3 +1,58 @@
+pub mod filesystem;
+
 fn main() {
+    keepalive_ops::start();
     println!("Hello, world!");
+}
+
+pub mod keepalive_ops {
+    use crate::filesystem;
+    use crate::models;
+    pub fn start() {
+        println!("start");
+    }
+    pub fn end() {}
+    pub fn get_status() {}
+}
+
+pub mod os_access {
+    pub fn get_info() {}
+}
+
+pub mod models {
+    use serde::{Deserialize, Serialize};
+    use serde_json;
+    use std::fmt;
+    pub type FsResult<T> = Result<T, std::io::Error>;
+
+    #[derive(Serialize, Deserialize)]
+    pub enum Status {
+        Alive,
+        Dead,
+        Broken,
+        Deploying,
+    }
+
+    impl fmt::Display for Status {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            match self {
+                Status::Alive => write!(f, "Alive"),
+                Status::Dead => write!(f, "Dead"),
+                Status::Broken => write!(f, "Broken"),
+                Status::Deploying => write!(f, "Deploying"),
+            }
+        }
+    }
+
+    #[derive(Serialize, Deserialize)]
+    pub struct ProcessData {
+        pid: String,
+        status: Status,
+    }
+
+    impl ProcessData {
+        pub fn to_output(&self) -> String {
+            serde_json::to_string(self).expect("should serialize process data always")
+        }
+    }
 }
