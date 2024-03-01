575cac7193c1c8ff380367e56ed71bd217e96ac9
astherath <farceriv@gmail.com>
2024-03-01 18:49:30 -0500

    added lint and larger modules

--- refactor/src/main.rs
+++ refactor/src/main.rs
@@ -1,12 +1,6 @@
-use localstore::query_data;
-use log::info;
-use models::{
-    db::{commands::Command, common::Id},
-    HandlerError,
-};
 
-use crate::{localstore::write_single, models::db::commands::CommandStatus};
-
+use models::HandlerError;
+use pre_event_loop::{get_device_id, get_user_id};
 mod models;
 
 #[tokio::main]
@@ -14,171 +8,134 @@ async fn main() -> Result<(), HandlerError> {
     std::env::set_var("RUST_LOG", "info");
     simple_logger::SimpleLogger::new().env().init().unwrap();
 
-    // prelim check for data
-    let user_id_key = "user_id";
-    let user_id = query_data(&user_id_key)?.expect("user id to be set");
-    info!("user_id retrieved from store is {}", user_id);
+    // pre event loop
+    let user_id = get_user_id()?;
+    let _device_id = get_device_id(&user_id).await?;
 
-    // get device id or register it if not set
-    let device_id_key = "device_id";
-    let device_id_resp = query_data(&device_id_key);
-    let device_id: Id;
-    if (&device_id_resp).is_err() {
-        device_id = register_device(&user_id).await?;
-        write_single(&device_id, device_id_key)?;
-    } else {
-        device_id = device_id_resp?.unwrap();
-    }
-    info!("device id retrieved from store is {}", device_id);
-
-    // get most recent command
-    let command = fetch_commands(&device_id).await?;
-    dbg!(&command);
-
-    // update_command_status_received(command).await?;
+    // run main event loop
 
     Ok(())
 }
 
-pub mod localstore {
-    use crate::models::HandlerError;
-    use jfs;
-    use log::{info, warn};
-    use std::collections::HashMap;
-use lazy_static::lazy_static;
-use std::sync::Mutex;
+pub mod pre_event_loop {
+    use log::info;
 
-    fn get_file_path() -> String {
-        "localstore.json".to_string()
+    use crate::{
+        api,
+        localstore::{query_data, write_single},
+        models::{db::common::Id, HandlerError},
+    };
+
+    /**
+     * main (pre-registered) run loop:
+     * 1. register device with server
+     * 2. test connection to server
+     */
+    pub fn get_device_name() -> String {
+        "testdevicefelipearce".to_string()
     }
 
-lazy_static! {
-    static ref HANDLE: Mutex<Result<jfs::Store, HandlerError>> = Mutex::new(get_handle());
-}
+    pub fn get_user_id() -> Result<Id, HandlerError> {
+        // prelim check for data
+        let user_id_key = "user_id";
+        let user_id = query_data(user_id_key)?.expect("user id to be set");
+        info!("user_id retrieved from store is {}", &user_id);
 
-    fn get_handle() -> Result<jfs::Store, HandlerError> {
-        let db = jfs::Store::new_with_cfg(
-            get_file_path(),
-            jfs::Config {
-                single: true,
-                pretty: true,
-                ..Default::default()
-            },
-        )?;
-        init_db(&db)?;
-        Ok(db)
+        Ok(user_id)
     }
 
-    fn init_db(db: &jfs::Store) -> Result<(), HandlerError> {
-        let key = "user_id";
-        let resp = query_internal(db, &key);
-        if resp.is_err() || resp?.is_none() {
-            let user_id = "ee9470de-54a4-419c-b34a-ba2fa18731d8";
-            db.save_with_id(&user_id.to_string(), &key)?;
-            info!("user_id set to default: {}", user_id);
+    pub async fn get_device_id(user_id: &Id) -> Result<Id, HandlerError> {
+        // get device id or register it if not set
+        let device_id_key = "device_id";
+        let device_id_resp = query_data(device_id_key);
+        let device_id: Id;
+        if device_id_resp.is_err() {
+            device_id = register_device(user_id).await?;
+            write_single(&device_id, device_id_key)?;
+        } else {
+            device_id = device_id_resp?.unwrap();
         }
+        info!("device id retrieved from store is {}", device_id);
+
+        Ok(device_id)
+    }
+
+    pub async fn register_device(user_id: &Id) -> Result<Id, HandlerError> {
+        let device_name = get_device_name();
+        info!("registering device with name: {}", device_name);
+
+        Ok(
+            api::requests::register_device::register_device(user_id, device_name)
+                .await?
+                .device_id,
+        )
+    }
+}
+
+pub mod main_event_loop {
+    use crate::{
+        api,
+        models::{
+            db::{
+                commands::{Command, CommandStatus},
+                common::Id,
+            },
+            HandlerError,
+        },
+    };
+
+    /**
+     * main (post-registered) run loop:
+     * 1. call server to fetch commands using the deviceId (TODO @felipearce: add some auth eventually)
+     *      a. if no commands found:
+     *          - sleep for foobar seconds and then redo loop
+     *
+     * 2. call server to update command status as executing/etc. and send ACK to server
+     * 3. execute command
+     * 4. call server to send outgoing update commands status request if success or err. or blocking or etc.
+     * 5. return data from command (if any)
+     */
+    pub async fn run_main_event_loop(device_id: &Id, _user_id: &Id) -> Result<(), HandlerError> {
+        // get most recent command
+        let command = fetch_commands(device_id).await?;
+        dbg!(&command);
+
+        // update_command_status_received(command).await?;
+        unimplemented!()
+    }
+
+    pub async fn fetch_commands(device_id: &Id) -> Result<Option<Command>, HandlerError> {
+        let response = api::requests::fetch_commands::fetch_commands(device_id.clone()).await?;
+        if response.is_none() {
+            Ok(None)
+        } else {
+            let command = response.unwrap().command;
+            Ok(Some(command))
+        }
+    }
+
+    pub fn fetch_next_command() {
+        unimplemented!()
+    }
+
+    pub fn ack_command_received() {
+        unimplemented!()
+    }
+
+    pub async fn update_command_status_received(command: Command) -> Result<(), HandlerError> {
+        let new_status = CommandStatus::Ready;
+        api::requests::update_command_status::update_command_status(&command, new_status).await?;
+
         Ok(())
     }
 
-    pub fn write_single(data: &String, key: &str) -> Result<(), HandlerError> {
-        info!("writing data for key: {}", key);
-        let binding = HANDLE.lock().unwrap();
-        let handle = binding.as_ref().unwrap();
-        handle.save_with_id(data, key)?;
-        Ok(())
+    pub fn execute_command() {
+        unimplemented!()
     }
 
-    pub fn write_data(data: HashMap<String, String>) -> Result<(), HandlerError> {
-        data.keys().for_each(|key| {
-            write_single(&data[key], key).unwrap();
-        });
-        Ok(())
+    pub fn update_command_status_after_execution() {
+        unimplemented!()
     }
-
-    fn query_internal(db: &jfs::Store, key: &str) -> Result<Option<String>, HandlerError> {
-        let data = db.get(key)?;
-        Ok(data)
-    }
-
-    pub fn query_data(key: &str) -> Result<Option<String>, HandlerError> {
-        info!("querying data for key: {}", key);
-        let binding = HANDLE.lock().unwrap();
-        let handle = binding.as_ref().unwrap();
-        query_internal(&handle, key)
-    }
-}
-
-/**
- * main (pre-registered) run loop:
- * 1. register device with server
- * 2. test connection to server
- */
-pub fn get_device_name() -> String {
-    "testdevicefelipearce".to_string()
-}
-
-pub async fn register_device(user_id: &Id) -> Result<Id, HandlerError> {
-    let device_name = get_device_name();
-    info!("registering device with name: {}", device_name);
-
-    Ok(
-        api::requests::register_device::register_device(user_id, device_name)
-            .await?
-            .device_id,
-    )
-}
-
-pub async fn test_connection() -> Result<(), HandlerError> {
-    unimplemented!()
-}
-
-/**
- * main (post-registered) run loop:
- * 1. call server to fetch commands using the deviceId (TODO @felipearce: add some auth eventually)
- *      a. if no commands found:
- *          - sleep for foobar seconds and then redo loop
- *
- * 2. call server to update command status as executing/etc. and send ACK to server
- * 3. execute command
- * 4. call server to send outgoing update commands status request if success or err. or blocking or etc.
- * 5. return data from command (if any)
- */
-pub fn run_main_event_loop() {
-    unimplemented!()
-}
-
-pub async fn fetch_commands(device_id: &Id) -> Result<Option<Command>, HandlerError> {
-    let response = api::requests::fetch_commands::fetch_commands(device_id.clone()).await?;
-    if response.is_none() {
-        return Ok(None);
-    }
-    else {
-        let command = response.unwrap().command;
-        Ok(Some(command))
-    }
-}
-
-pub fn fetch_next_command() {
-    unimplemented!()
-}
-
-pub fn ack_command_received() {
-    unimplemented!()
-}
-
-pub async fn update_command_status_received(command: Command) -> Result<(), HandlerError> {
-    let new_status = CommandStatus::Ready;
-    api::requests::update_command_status::update_command_status(&command, new_status).await?;
-
-    Ok(())
-}
-
-pub fn execute_command() {
-    unimplemented!()
-}
-
-pub fn update_command_status_after_execution() {
-    unimplemented!()
 }
 
 // no call needed to return data
@@ -202,7 +159,7 @@ pub mod api {
         }
 
         pub mod fetch_commands {
-            use crate::models::db::{commands::Command, common::Id};
+            use crate::models::db::{commands::Command};
             use serde::{Deserialize, Serialize};
 
             #[derive(Serialize, Deserialize, Debug)]
@@ -326,7 +283,9 @@ pub mod api {
                 format!("{}/commands/recent", host)
             }
 
-            pub async fn fetch_commands(device_id: Id) -> ApiResult<Option<FetchRecentCommandResponse>> {
+            pub async fn fetch_commands(
+                device_id: Id,
+            ) -> ApiResult<Option<FetchRecentCommandResponse>> {
                 let url = get_url();
 
                 let response = get_client()
@@ -347,5 +306,73 @@ pub mod api {
                 Ok(Some(json))
             }
         }
+    }
+}
+
+pub mod localstore {
+    use crate::models::HandlerError;
+    use jfs;
+    use lazy_static::lazy_static;
+    use log::{info};
+    use std::collections::HashMap;
+    use std::sync::Mutex;
+
+    fn get_file_path() -> String {
+        "localstore.json".to_string()
+    }
+
+    lazy_static! {
+        static ref HANDLE: Mutex<Result<jfs::Store, HandlerError>> = Mutex::new(get_handle());
+    }
+
+    fn get_handle() -> Result<jfs::Store, HandlerError> {
+        let db = jfs::Store::new_with_cfg(
+            get_file_path(),
+            jfs::Config {
+                single: true,
+                pretty: true,
+                ..Default::default()
+            },
+        )?;
+        init_db(&db)?;
+        Ok(db)
+    }
+
+    fn init_db(db: &jfs::Store) -> Result<(), HandlerError> {
+        let key = "user_id";
+        let resp = query_internal(db, key);
+        if resp.is_err() || resp?.is_none() {
+            let user_id = "ee9470de-54a4-419c-b34a-ba2fa18731d8";
+            db.save_with_id(&user_id.to_string(), key)?;
+            info!("user_id set to default: {}", user_id);
+        }
+        Ok(())
+    }
+
+    pub fn write_single(data: &String, key: &str) -> Result<(), HandlerError> {
+        info!("writing data for key: {}", key);
+        let binding = HANDLE.lock().unwrap();
+        let handle = binding.as_ref().unwrap();
+        handle.save_with_id(data, key)?;
+        Ok(())
+    }
+
+    pub fn write_data(data: HashMap<String, String>) -> Result<(), HandlerError> {
+        data.keys().for_each(|key| {
+            write_single(&data[key], key).unwrap();
+        });
+        Ok(())
+    }
+
+    fn query_internal(db: &jfs::Store, key: &str) -> Result<Option<String>, HandlerError> {
+        let data = db.get(key)?;
+        Ok(data)
+    }
+
+    pub fn query_data(key: &str) -> Result<Option<String>, HandlerError> {
+        info!("querying data for key: {}", key);
+        let binding = HANDLE.lock().unwrap();
+        let handle = binding.as_ref().unwrap();
+        query_internal(handle, key)
     }
 }
