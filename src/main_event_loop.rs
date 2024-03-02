c1e57a88f0d0a2c02dc1b809f47c42ef7aea286a
astherath <farceriv@gmail.com>
2024-03-02 01:15:41 -0500

    moved out for simpler structure

--- src/main_event_loop.rs
+++ src/main_event_loop.rs
@@ -134,13 +134,15 @@ pub mod executor {
             CommandNames::Test => {
                 // TODO @felipearce: add test command here
                 Ok(Some("test".to_string()))
-            },
+            }
             CommandNames::ShellCmd => {
                 // execute args in the shell
                 let args = match command.args.as_ref() {
                     Some(args) => args,
                     None => {
-                        return Err(HandlerError::ParseError("no args found for shell cmd".to_string()));
+                        return Err(HandlerError::ParseError(
+                            "no args found for shell cmd".to_string(),
+                        ));
                     }
                 };
 
@@ -153,12 +155,10 @@ pub mod executor {
                     Ok(output) => {
                         let output_str = String::from_utf8(output.stdout).unwrap();
                         Ok(Some(output_str))
-                    },
-                    Err(e) => {
-                        Err(HandlerError::CmdError(e.to_string()))
-                    },
+                    }
+                    Err(e) => Err(HandlerError::CmdError(e.to_string())),
                 }
-            },
+            }
             _ => {
                 // TODO @felipearce: add more commands here
                 Ok(None)
