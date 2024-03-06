8eca05c049711f35a7819f95e0853a2003627035
astherath <farceriv@gmail.com>
2024-03-06 10:31:36 -0500

    split into files

---
+++ src/api/requests/mod.rs
@@ -0,0 +1,117 @@
+pub mod fetch_commands;
+pub mod register_device;
+pub mod update_command_status;
+
+use crate::models::HandlerError;
+use futures::future::BoxFuture;
+use log::{error, warn};
+use reqwest::StatusCode;
+pub type ApiResult<T> = Result<T, HandlerError>;
+
+fn get_client() -> reqwest::Client {
+    reqwest::Client::new()
+}
+
+pub struct ApiConfig {
+    pub host: String,
+    pub port: Option<u16>,
+}
+
+impl ApiConfig {
+    pub fn new(host: String, port: Option<u16>) -> Self {
+        ApiConfig { host, port }
+    }
+
+    fn get_port_string_if_any(&self) -> String {
+        match self.port {
+            Some(val) => format!(":{}", &val),
+            None => "".to_string(),
+        }
+    }
+
+    pub fn with_path(&self, path: &str) -> String {
+        let port_string = self.get_port_string_if_any();
+        format!("{}{}{}", self.host, port_string, path)
+    }
+}
+
+impl Default for ApiConfig {
+    fn default() -> Self {
+        ApiConfig {
+            host: "localhost".to_string(),
+            port: Some(5001),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::api::requests::ApiConfig;
+
+    #[test]
+    fn test_api_config_with_path() {
+        let host = "testhost".to_string();
+        let port = Some(5001);
+        let config = ApiConfig::new(host, port);
+
+        let path = "/testpath";
+        let result = config.with_path(path);
+
+        let expected = "testhost:5001/testpath";
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_api_config_with_path_no_port() {
+        let host = "testhost".to_string();
+        let port = None;
+        let config = ApiConfig::new(host, port);
+
+        let path = "/testpath";
+        let result = config.with_path(path);
+
+        let expected = "testhost/testpath";
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_default_api_config_with_path() {
+        let config = ApiConfig::default();
+
+        let path = "/testpath";
+        let result = config.with_path(path);
+
+        let expected = "localhost:5001/testpath";
+        assert_eq!(result, expected);
+    }
+}
+
+async fn handle_response<T>(
+    response: reqwest::Response,
+    on_ok: impl Fn(reqwest::Response) -> BoxFuture<'static, Result<T, HandlerError>>,
+) -> Result<T, HandlerError> {
+    let status = response.status();
+    if let StatusCode::OK = status {
+        Ok(on_ok(response).await?)
+    } else {
+        let text = response.text().await?;
+        match status {
+            StatusCode::NOT_FOUND => {
+                warn!("No commands found: {}", &text);
+                Err(HandlerError::NotFound)
+            }
+            StatusCode::INTERNAL_SERVER_ERROR => {
+                error!("server error on fetch: {}", &text);
+                Err(HandlerError::ServerError)
+            }
+            StatusCode::BAD_REQUEST | StatusCode::UNPROCESSABLE_ENTITY => {
+                warn!("error in data passed in: {}", &text);
+                Err(HandlerError::InputError)
+            }
+            _ => {
+                warn!("unknown error code: {}, {}", &text, status);
+                Err(HandlerError::ApiError)
+            }
+        }
+    }
+}
---
+++ src/api/requests/update_command_status.rs
@@ -0,0 +1,116 @@
+
+use futures::future::BoxFuture;
+
+use crate::api::models::update_command_status::UpdateCommandStatusRequest;
+use crate::api::requests::{get_client, ApiResult};
+use crate::models::db::commands::{Command, CommandStatus};
+use crate::models::db::common::HasId;
+
+use super::{handle_response, ApiConfig};
+
+pub async fn update_command_status(
+    command: &Command,
+    new_status: CommandStatus,
+    config: &ApiConfig,
+) -> ApiResult<()> {
+    let request = UpdateCommandStatusRequest {
+        command_id: command.get_id().clone(),
+        status: new_status,
+    };
+
+    let url = config.with_path("/commands/update/status");
+
+    let response = get_client().patch(url).json(&request).send().await?;
+
+    let bind = |_: reqwest::Response| -> BoxFuture<'static, ApiResult<()>> {
+        Box::pin(async move { Ok(()) })
+    };
+
+    handle_response(response, bind).await
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{
+        api::models::update_command_status::UpdateCommandStatusRequest,
+        models::{
+            db::{
+                commands::{Command, CommandStatus},
+                common::{HasId, Id},
+            },
+            HandlerError,
+        },
+        test_commons::{before_each, get_404_json_string, get_500_json_string, setup_server},
+    };
+
+    fn get_json_payload(command_id: Id) -> (UpdateCommandStatusRequest, String) {
+        let data = UpdateCommandStatusRequest::new(command_id);
+        let data_string = serde_json::to_string(&data).unwrap();
+
+        (data, data_string)
+    }
+
+    #[tokio::test]
+    async fn test_update_command() {
+        before_each();
+
+        let command = Command::default();
+        let (_, json) = get_json_payload(command.get_id().clone());
+        let (mut server, config) = setup_server();
+
+        let mock = server
+            .mock("PATCH", "/commands/update/status")
+            .with_status(200)
+            .with_body(json)
+            .create();
+
+        let new_status = CommandStatus::Terminated;
+        let result = super::update_command_status(&command, new_status, &config).await;
+
+        assert!(result.is_ok());
+        result.unwrap();
+        mock.assert();
+    }
+
+    #[tokio::test]
+    async fn test_update_commands_404_fail() {
+        before_each();
+
+        let command = Command::default();
+        let (mut server, config) = setup_server();
+
+        let mock = server
+            .mock("PATCH", "/commands/update/status")
+            .with_status(404)
+            .with_body(get_404_json_string())
+            .create();
+
+        let new_status = CommandStatus::Terminated;
+        let result = super::update_command_status(&command, new_status, &config).await;
+
+        assert!(result.is_err());
+        assert!(matches!(result.err().unwrap(), HandlerError::NotFound));
+        mock.assert();
+    }
+
+    #[tokio::test]
+    async fn test_update_commands_500_fail() {
+        before_each();
+
+        let command = Command::default();
+        let (mut server, config) = setup_server();
+
+        let mock = server
+            .mock("PATCH", "/commands/update/status")
+            .with_status(500)
+            .with_body(get_500_json_string())
+            .create();
+
+        let new_status = CommandStatus::Terminated;
+        let result = super::update_command_status(&command, new_status, &config).await;
+
+        assert!(result.is_err());
+        assert!(matches!(result.err().unwrap(), HandlerError::ServerError));
+        mock.assert();
+    }
+}
